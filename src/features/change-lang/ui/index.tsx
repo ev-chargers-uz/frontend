6136ef3a8db0b22d138a51690dc3648989f64b8d
KIRA <bfakhruz@gmail.com>
2023-02-16 17:55:35 +0500

    styled change-lang feature

--- src/features/change-lang/ui/index.tsx
+++ src/features/change-lang/ui/index.tsx
@@ -1,5 +1,14 @@
+import styles from "./styles.module.scss";
+
 export const ChangeLang = () => {
 	return (
-		<div>change lang</div>
+		<div class={styles.wrapper}>
+			<img src="./icons/lang.svg" alt="icon" />
+			<select class={styles.select}>
+				<option value="en">Eng</option>
+				<option value="ru">Rus</option>
+				<option value="uz">Uzb</option>
+			</select>
+		</div>
 	)
 }
