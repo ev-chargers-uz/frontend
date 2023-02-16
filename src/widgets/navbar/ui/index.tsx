ee1483266d891d34046072afcd02b588eba0f394
KIRA <bfakhruz@gmail.com>
2023-02-16 17:53:48 +0500

    styled navbar widget

--- src/widgets/navbar/ui/index.tsx
+++ src/widgets/navbar/ui/index.tsx
@@ -1,5 +1,25 @@
+import styles from "./styles.module.scss";
+
 export const Navbar = () => {
 	return (
-		<nav>navbar</nav>
+		<nav>
+			<ul class={styles.navList}>
+				<li class={styles.listItem}>
+					<a href="/products" class={styles.link}>Products</a>
+				</li>
+				<li class={styles.listItem}>
+					<a href="/drivers" class={styles.link}>Drivers</a>
+				</li>
+				<li class={styles.listItem}>
+					<a href="/business" class={styles.link}>Business</a>
+				</li>
+				<li class={styles.listItem}>
+					<a href="/about" class={styles.link}>About us</a>
+				</li>
+				<li class={styles.listItem}>
+					<a href="/blog" class={styles.link}>Blog</a>
+				</li>
+			</ul>
+		</nav>
 	)
 }
