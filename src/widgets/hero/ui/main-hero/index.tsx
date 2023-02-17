ed260654bfb6597def57149fc80f53c233b9eb79
KIRA <bfakhruz@gmail.com>
2023-02-17 15:32:10 +0500

    edited hero widget

--- src/widgets/hero/ui/main-hero/index.tsx
+++ src/widgets/hero/ui/main-hero/index.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@shared/ui";
+import { Container, LinkButton } from "@shared/ui";
 import styles from "./styles.module.scss";
 
 export const MainHero = () => {
@@ -9,7 +9,7 @@ export const MainHero = () => {
 			<div class={styles.wrapper}>
 				<h1 class={styles.title}>Charge your car with us with faster and safe way</h1>
 				<p class={styles.text}>Charge up faster with our home EV chargers easy to install, smart features, robust & reliable, and can charge all electric vehicles</p>
-				<button>Learn more</button>
+				<LinkButton>Learn more</LinkButton>
 			</div>
 
 			</Container>
