bc4d14be189bb852b655759b5c1ff4828f66858c
KIRA <bfakhruz@gmail.com>
2023-02-17 14:16:21 +0500

    styled hero widget

--- src/widgets/hero/ui/main-hero/index.tsx
+++ src/widgets/hero/ui/main-hero/index.tsx
@@ -1,5 +1,15 @@
+import styles from "./styles.module.scss";
+
 export const MainHero = () => {
 	return (
-		<div>main hero</div>
+		<section>
+
+			<div class={styles.wrapper}>
+				<h1 class={styles.title}>Charge your car with us with faster and safe way</h1>
+				<p class={styles.text}>Charge up faster with our home EV chargers easy to install, smart features, robust & reliable, and can charge all electric vehicles</p>
+				<button>Learn more</button>
+			</div>
+
+		</section>
 	)
 }
