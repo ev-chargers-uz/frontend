d9e2ee9fc62423f04a64693b4d095b0322dd001a
KIRA <bfakhruz@gmail.com>
2023-02-18 14:15:03 +0500

    updated blocks widget

---
+++ src/widgets/blocks/ui/block2/index.tsx
@@ -0,0 +1,18 @@
+import { Container, LinkButton } from "@shared/ui";
+import styles from "./styles.module.scss";
+
+export const Block2 = () => {
+	return (
+		<section class={styles.section}>
+			<Container>
+
+				<div class={styles.wrapper}>
+					<h2 class={styles.title}>Business solutions</h2>
+					<p class={styles.text}>Our chargers are intelligent, durable, and quietly charging electric vehicles for decades to come. We offer a full range of deployment configurations and integrated management platform</p>
+					<LinkButton>Learn more</LinkButton>
+				</div>
+
+			</Container>
+		</section>
+	)
+}
---
+++ src/widgets/blocks/ui/block3/index.tsx
@@ -0,0 +1,18 @@
+import { Container, LinkButton } from "@shared/ui";
+import styles from "./styles.module.scss";
+
+export const Block3 = () => {
+	return (
+		<section class={styles.section}>
+			<Container>
+
+				<div class={styles.wrapper}>
+					<h2 class={styles.title}>App for customers</h2>
+					<p class={styles.text}>Take control of your charging costs across all locations, monitor monthly charger usage, and every detail & stats of your EV charging needs in app <br /> All this, free of charge</p>
+					<LinkButton>Download</LinkButton>
+				</div>
+
+			</Container>
+		</section>
+	)
+}
