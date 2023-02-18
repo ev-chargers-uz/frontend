bcad3aeebb34bb50c8344b0b5705e5e21e5211c7
KIRA <bfakhruz@gmail.com>
2023-02-18 17:49:04 +0500

    edited link-button shared/ui component

--- src/shared/ui/link-button/index.tsx
+++ src/shared/ui/link-button/index.tsx
@@ -4,15 +4,22 @@ import styles from "./styles.module.scss";
 export interface LinkButtonProps {
 	children: string,
 	link?: string,
-	target?: "_blank" | "_self";
+	target?: "_blank" | "_self",
+	type?: "primary" | "secondary",
 }
 
 export const LinkButton = (props: LinkButtonProps) => {
-	const merged = mergeProps( {target: "_self"}, props );
+	const merged = mergeProps( {target: "_self", type: "primary"}, props );
 
-	return (
-		<a href={props.link} target={merged.target} rel="noopener" class={styles.linkButton}>
+	if (merged.type === "primary") return (
+		<a href={props.link} target={merged.target} rel="noopener" class={styles.linkButtonPrimary}>
 			{props.children}
 		</a>
-	)
+	);
+
+	if (merged.type === "secondary") return (
+		<a href={props.link} target={merged.target} rel="noopener" class={styles.linkButtonSecondary}>
+			{props.children}
+		</a>
+	);
 }
