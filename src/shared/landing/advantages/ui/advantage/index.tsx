ff6c691d91e616b932e0c2fd26bd7450bec6b326
KIRA <bfakhruz@gmail.com>
2023-02-18 16:51:22 +0500

    styled advantages landing components

--- src/shared/landing/advantages/ui/advantage/index.tsx
+++ src/shared/landing/advantages/ui/advantage/index.tsx
@@ -1,5 +1,17 @@
-export const Advantage = () => {
+import styles from "./styles.module.scss";
+
+export interface AdvantageProps {
+	icon: string,
+	title: string,
+	text: string,
+}
+
+export const Advantage = (props: AdvantageProps) => {
 	return (
-		<div>advantage</div>
+		<div class={styles.advantage}>
+			<img src={props.icon} alt="icon" class={styles.icon} />
+			<h4 class={styles.title}>{props.title}</h4>
+			<p class={styles.text}>{props.text}</p>
+		</div>
 	)
 }
--- src/shared/landing/advantages/ui/index.tsx
+++ src/shared/landing/advantages/ui/index.tsx
@@ -1,18 +1,33 @@
+import { Container } from "@shared/ui";
 import { Advantage } from "./advantage";
 import styles from "./styles.module.scss";
 
 export const Advantages = () => {
 	return (
 		<section>
+			<Container>
 
-			<h3 class={styles.title}>Our advantages</h3>
-			<div class={styles.advantages}>
-				<Advantage />
-				<Advantage />
-				<Advantage />
-			</div>
-			<button>Learn more</button>
+				<h3 class={styles.title}>Our advantages</h3>
+				<div class={styles.advantages}>
+					<Advantage
+						icon="/icons/icon01.png"
+						title="Over the country"
+						text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+					/>
+					<Advantage
+						icon="/icons/icon02.png"
+						title="NFC Payment"
+						text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+					/>
+					<Advantage
+						icon="/icons/icon03.png"
+						title="Super Fast Chargers"
+						text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+					/>
+				</div>
+				<button>Learn more</button>
 
+			</Container>
 		</section>
 	)
 }
