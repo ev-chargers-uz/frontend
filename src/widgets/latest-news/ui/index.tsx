4b2961c0a2064ef01ce761facf10733780866c23
KIRA <bfakhruz@gmail.com>
2023-02-22 16:49:06 +0500

    edited latest-news widget

--- src/widgets/latest-news/ui/index.tsx
+++ src/widgets/latest-news/ui/index.tsx
@@ -1,7 +1,6 @@
 import { Container, LinkButton } from "@shared/ui";
 import { NewsCard, newsConfig } from "@entities/news";
 import styles from "./styles.module.scss";
-import { For } from "solid-js";
 
 export const LatestNews = () => {
 	return (
@@ -9,16 +8,18 @@ export const LatestNews = () => {
 			<section class={styles.section}>
 				<h3 class={styles.title}>The latest news</h3>
 				<div class={styles.news}>
-
-					<For each={newsConfig.news.slice(-3)}>{ (news, index) =>
-						<NewsCard
-							image={news.image}
-							title={news.title}
-							description={news.description}
-							link={news.link}
-						/>
-					}</For>
-
+					{
+						newsConfig.news.slice(-3).map( (news) => {
+							return (
+								<NewsCard
+									image={news.image}
+									title={news.title}
+									description={news.description}
+									link={news.link}
+								/>
+							)
+						})
+					}
 				</div>
 				<LinkButton type="secondary">Learn more</LinkButton>
 			</section>
