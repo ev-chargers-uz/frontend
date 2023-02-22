4be6acd36f551eb2fa0f6341e11aa44d9d4df972
KIRA <bfakhruz@gmail.com>
2023-02-22 16:49:32 +0500

    edited latest-faq widget

--- src/widgets/latest-faq/ui/index.tsx
+++ src/widgets/latest-faq/ui/index.tsx
@@ -1,5 +1,25 @@
+import { FaqCard, faqConfig } from "@entities/faq";
+import { Container } from "@shared/ui";
+import styles from "./styles.module.scss";
+
 export const LatestFaq = () => {
 	return (
-		<section>latest faq</section>
+		<Container>
+			<section>
+				<h3 class={styles.title}>Frequently Asked Questions</h3>
+				<div class={styles.faqs}>
+					{
+						faqConfig.faqList.slice(-3).map( (faq) => {
+							return (
+								<FaqCard
+									question={faq.question}
+									answer={faq.answer}
+								/>
+							)
+						})
+					}
+				</div>
+			</section>
+		</Container>
 	)
 }
