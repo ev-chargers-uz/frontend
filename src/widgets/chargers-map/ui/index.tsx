b11e83bdcaed807f6ec2e396b0873a628d92d4ea
KIRA <bfakhruz@gmail.com>
2023-02-25 14:51:22 +0500

    edited chargers-map

--- src/widgets/chargers-map/ui/index.tsx
+++ src/widgets/chargers-map/ui/index.tsx
@@ -1,12 +1,12 @@
-import { Map } from "@entities/map"
-import { Container } from "@shared/ui"
+import { Container } from "@shared/ui";
+import styles from "./styles.module.scss";
 
 export const ChargersMap = () => {
 	return (
 		<Container>
 			<section>
-				<h3>Map with  our chargers</h3>
-				<Map />
+				<h3 class={styles.title}>Map with  our chargers</h3>
+				<img src="/images/map01.png" alt="map" class={styles.map} />
 			</section>
 		</Container>
 	)
