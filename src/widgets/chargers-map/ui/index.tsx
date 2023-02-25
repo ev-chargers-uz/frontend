aaa00fa9c93e85b65dfcac002125bd99c1957002
KIRA <bfakhruz@gmail.com>
2023-02-25 14:33:33 +0500

    added chargers-map widget

---
+++ src/widgets/chargers-map/ui/index.tsx
@@ -0,0 +1,13 @@
+import { Map } from "@entities/map"
+import { Container } from "@shared/ui"
+
+export const ChargersMap = () => {
+	return (
+		<Container>
+			<section>
+				<h3>Map with  our chargers</h3>
+				<Map />
+			</section>
+		</Container>
+	)
+}
