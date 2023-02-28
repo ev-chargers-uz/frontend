9583c78446c24ee386cc9b40e62e63f263f0df39
KIRA <bfakhruz@gmail.com>
2023-02-28 16:14:02 +0500

    edited footer widget

--- src/widgets/footer/ui/footer-bottom/index.tsx
+++ src/widgets/footer/ui/footer-bottom/index.tsx
@@ -1,5 +1,16 @@
+import { config } from "@shared/config";
+import styles from "./styles.module.scss";
+
 export const FooterBottom = () => {
 	return (
-		<div></div>
+		<div class={styles.footerBottom}>
+			<p class={styles.text}>EV ON. {new Date().getFullYear()}</p>
+			<div class={styles.logosWrapper}>
+				<a href={config.company.facebook} target="_blank" rel="noopener"><img src="/icons/facebook.svg" alt="icon" class={styles.logo} /></a>
+				<a href={config.company.youtube} target="_blank" rel="noopener"><img src="/icons/youtube.svg" alt="icon" class={styles.logo} /></a>
+				<a href={config.company.instagram} target="_blank" rel="noopener"><img src="/icons/instagram.svg" alt="icon" class={styles.logo} /></a>
+				<a href={config.company.twitter} target="_blank" rel="noopener"><img src="/icons/twitter.svg" alt="icon" class={styles.logo} /></a>
+			</div>
+		</div>
 	)
 }
